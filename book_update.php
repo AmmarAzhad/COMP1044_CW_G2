a77651065d6764348407b6f596a6378baad79241
LohXH <hfyxl4@nottingham.edu.my>
2022-04-13 00:06:17 +0800

    create book_update.php
    not done yet

---
+++ book_update.php
@@ -0,0 +1,119 @@
+<html>
+<head>
+<link rel="stylesheet" href="design.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<title>LIBRARY Read 2gether</title>
+<style>
+table {
+	border: none;
+	font-family: Tw Cen MT;
+	font-size: 20px;
+}
+</style>
+
+<body>
+
+<form action="book_add_back.php" method="POST">
+
+<div class="content-container-box">
+<div style="display: flex">
+<div style="width: 500px;">
+<div class="book-contents" style="border: 1px #000 solid; border-radius: 20px;  padding: 30px">
+
+<table cellpadding=6px>
+<tr>
+<td></td>
+<td></td>
+<td style="width: 30px"></td>
+</tr>
+
+<tr>
+<td></td>
+<td> Book ID:</td>
+<td><input class = "input" type="text" name="i_bookid" required></td>
+</div>
+<td></td>
+</tr>
+
+<tr>
+<td></td>
+<td> Book Title:</td>
+<div class="search">
+<td><input class = "input" type="text" placeholder="Fantastic Pets" name="i_booktitle" required></td>
+</div>
+<td></td>
+</tr>
+
+<tr>
+<td></td>
+<td>Author :</td>
+<td><input type="text" name="i_author" required></td>
+<td></td>
+</tr>
+
+<tr>
+<td></td>
+<td>Book Copies :</td>
+<td><input type="text" name="i_bookcopies" required></td>
+<td></td>
+</tr>
+<tr>
+<tr>
+<td></td>
+<td>Book Publisher Company :</td>
+<td><input type="text" placeholder="Regency Publishing Group" name="i_bookpub" required></td>
+<td></td>
+</tr>
+<tr>
+<td></td>
+<td>Publisher Name :</td>
+<td><input type="text" placeholder="0174371312" name="'i_pubname" required></td>
+<td></td>
+</tr>
+<tr>
+<td></td>
+<td>ISBN :</td>
+<td><input type="text" placeholder="0-12-345678-9" name="i_isbn" required></td>
+<td></td>
+</tr>
+<tr>
+<td></td>
+<td>Copyright Year :</td>
+<td><input type="text" name="i_copyrightyear" required></td>
+<td></td>
+</tr>
+<tr>
+<td></td>
+<td>Date Added :</td>
+<td><input type="date" placeholder="0174371312" name="i_dateadded" required></td>
+<td></td>
+</tr>
+<tr>
+<td></td>
+<td>Status :</td>
+<td><select name = "i_status">
+	<option value = "New">New</option>
+	<option value = "Old">Old</option>
+	<option value = "Damaged">Damaged</option>
+	<option value = "Lost">Lost</option>
+	<option value = "Archieved">Archieved</option>
+</td>
+</tr>
+<tr>
+<td></td>
+<td></td>
+<td><input type="submit" name="loginBtn" value="Modify"></td>
+<td></td>
+</tr>
+
+</table>
+
+</div>
+</div>
+</div>
+</div>
+
+</form>
+</div>
+</body>
+</html>
