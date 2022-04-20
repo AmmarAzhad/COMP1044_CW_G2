3b292624dd6f914b001fd0791558e64c53e91296
LohXH <hfyxl4@nottingham.edu.my>
2022-04-20 13:57:51 +0800

    fixed book_update

--- book_update.php
+++ book_update.php
@@ -55,8 +55,8 @@ include ("header.html");
 
 
 if(isset($_POST['update'])){
+    $id = $_GET['updateid'];
 $book_title=$_POST['i_booktitle'];
-$category_id =$_POST['i_categoryid'];
 $author =$_POST['i_author'];
 $book_copies =$_POST['i_bookcopies'];
 $book_pub =$_POST['i_bookpub'];
@@ -67,7 +67,7 @@ $date_added =$_POST['i_dateadded'];
 $status =$_POST['i_status'];
 
 //Simpan data dalam DB
-$mysql = "UPDATE `book` SET i_booktitle='$book_title', i_categoryid='$category_id', i_author='$author', i_bookcopies='$book_copies', i_bookpub='$book_pub', i_pubname='$publisher_name', i_isbn='$isbn', i_copyrightyear='$copyright_year', i_dateadded='$date_added' , i_status='$status'";
+$mysql = "UPDATE book SET book_title='$book_title', author='$author', book_copies='$book_copies', book_pub='$book_pub', isbn='$isbn', copyright_year='$copyright_year', date_added='$date_added' , status='$status' WHERE book_id = $id";
 if (mysqli_query($conn, $mysql)) {
 	//papar javascript alert jika pengguna baru berjaya daftar
 	echo '<script type="text/javascript">;
@@ -82,6 +82,22 @@ else {
 }
 ?>
 
+<?php
+if (isset($_GET['updateid'])){
+  $id = $_GET['updateid'];
+  $query = "SELECT * FROM book WHERE book_id = $id";
+  $mysql = $query;
+  $result = mysqli_query($conn, $mysql) or die(mysql_error());
+  $row = mysqli_fetch_assoc($result);
+  $book_title = $row['book_title'];
+  $author = $row['author'];
+  $book_copies = $row['book_copies'];
+  $book_pub = $row['book_pub'];
+  $publisher_name = $row['publisher_name'];
+  $isbn = $row['isbn'];
+  $copyright_year = $row['copyright_year'];
+  $date = $row['date_added'];
+}
 ?>
 
 
@@ -90,6 +106,7 @@ else {
 
 <div id="title"><p>Library Read 2gether<p>Update Book Details</div>
 
+<form method="POST">
 <div class="container center">
 <div style="display: flex">
 <div style="margin-left: 400px;">
@@ -109,7 +126,8 @@ else {
 <td></td>
 <td> Book Title:</td>
 <div class="search">
-<td><input class = "input" type="text" placeholder="Fantastic Pets" name="i_booktitle" required></td>
+<td><input class = "input" type="text" name="i_booktitle" required value="<?php echo $book_title?>">
+</td>
 </div>
 <td></td>
 </tr>
@@ -117,45 +135,45 @@ else {
 <tr>
 <td></td>
 <td>Author :</td>
-<td><input type="text" name="i_author" required></td>
+<td><input type="text" name="i_author" required value="<?php echo $author?>"></td>
 <td></td>
 </tr>
 
 <tr>
 <td></td>
 <td>Book Copies :</td>
-<td><input type="text" name="i_bookcopies" required></td>
+<td><input type="text" name="i_bookcopies" required value="<?php echo $book_copies?>"></td>
 <td></td>
 </tr>
 <tr>
 <tr>
 <td></td>
 <td>Book Publisher Company :</td>
-<td><input type="text" placeholder="Regency Publishing Group" name="i_bookpub" required></td>
+<td><input type="text" name="i_bookpub" required value="<?php echo $book_pub?>"></td>
 <td></td>
 </tr>
 <tr>
 <td></td>
 <td>Publisher Name :</td>
-<td><input type="text" placeholder="0174371312" name="'i_pubname" required></td>
+<td><input type="text" name="i_pubname" required value="<?php echo $publisher_name?>"></td>
 <td></td>
 </tr>
 <tr>
 <td></td>
 <td>ISBN :</td>
-<td><input type="text" placeholder="0-12-345678-9" name="i_isbn" required></td>
+<td><input type="text" name="i_isbn" required value="<?php echo $isbn?>"></td>
 <td></td>
 </tr>
 <tr>
 <td></td>
 <td>Copyright Year :</td>
-<td><input type="text" name="i_copyrightyear" required></td>
+<td><input type="text" name="i_copyrightyear" required value="<?php echo $copyright_year?>"></td>
 <td></td>
 </tr>
 <tr>
 <td></td>
 <td>Date Added :</td>
-<td><input type="date" placeholder="0174371312" name="i_dateadded" required></td>
+<td><input type="datetime-local" placeholder="0174371312" name="i_dateadded" required value="<?php echo $date?>"></td>
 <td></td>
 </tr>
 <tr>
@@ -172,7 +190,7 @@ else {
 <tr>
 <td></td>
 <td></td>
-<td><input type="submit" value="Update" name="update" class="button1" formmethod="POST" formaction="book.php"></td>
+<td><input type="submit" value="Update" name="update" class="button1" updateid="$id"></td>
 <td><button class="button2"><a href="book.php" style="color:white">Cancel</a></button></td>
 <td></td>
 </tr>
