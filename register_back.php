3bb65b4518f146d3425286dd4d5a14349563d4c5
enxuantan1030 <103411614+enxuantan1030@users.noreply.github.com>
2022-04-24 15:42:26 +0800

    Update register_back.php

--- register_back.php
+++ register_back.php
@@ -3,7 +3,6 @@
 include ('db_conn.php');
 
 //get all inputs from user in register.php
-$user_id = $_POST['i_userid'];
 $username = $_POST['i_username'];
 $password = md5($_POST['i_password']);
 $firstname = $_POST['i_firstname'];
@@ -11,8 +10,8 @@ $lastname = $_POST['i_lastname'];
 
 //Insert user inputs into Database
 $mysql = "INSERT INTO users
-(user_id, username, password, firstname, lastname)
-		VALUES ('$user_id', '$username', '$password', '$firstname','$lastname')";
+(username, password, firstname, lastname)
+		VALUES ('$username', '$password', '$firstname','$lastname')";
 if (mysqli_query($conn, $mysql)) {
 	//Display pop-up box that the user has register successfully 
 	echo '<script type="text/javascript">
